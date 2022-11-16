b9d0a9f74297b955c00d44f7e4d4dd51b0c27424
Engineer <arsenya.suvorov@mail.ru>
2022-11-16 14:49:23 +0400

    вторая задача

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA > numberB)
+    {
+        Console.WriteLine("max="+numberA);
+    }
+else
+    {
+       Console.WriteLine("max="+numberB);
+    }
+
